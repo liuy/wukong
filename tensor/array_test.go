09dd3d39f0588885388e5fbddcd8e0563a0ff97a
Liu Yuan <namei.unix@gmail.com>
2024-11-20 15:09:21 +0800

    array_test: use assert
    Signed-off-by: Liu Yuan <namei.unix@gmail.com>

--- tensor/array_test.go
+++ tensor/array_test.go
@@ -2,8 +2,9 @@ package tensor
 
 import (
 	"fmt"
-	"math"
 	"testing"
+
+	"github.com/liuy/wukong/assert"
 )
 
 func TestMain(m *testing.M) {
@@ -15,9 +16,7 @@ func TestMain(m *testing.M) {
 func TestShapeLen(t *testing.T) {
 	s := Shape{2, 3, 4}
 	expected := 24
-	if s.Len() != expected {
-		t.Errorf("Shape.Len() = %d, want %d", s.Len(), expected)
-	}
+	assert.Equal(t, s.Len(), expected)
 }
 
 func TestArrayElemSize(t *testing.T) {
@@ -32,12 +31,9 @@ func TestArrayElemSize(t *testing.T) {
 
 	for _, tt := range tests {
 		a, err := MakeArrayFrom(tt.shape, tt.data)
-		if err != nil {
-			t.Fatalf("MakeArrayFrom() error = %v", err)
-		}
-		if a.ElemSize() != tt.expected {
-			t.Errorf("Array.ElemSize() = %d, want %d", a.ElemSize(), tt.expected)
-		}
+		assert.NoErr(t, err)
+		assert.NotNil(t, a)
+		assert.Equal(t, a.ElemSize(), tt.expected)
 	}
 }
 
@@ -53,31 +49,23 @@ func TestArraySize(t *testing.T) {
 
 	for _, tt := range tests {
 		a, err := MakeArrayFrom(tt.shape, tt.data)
-		if err != nil {
-			t.Fatalf("MakeArrayFrom() error = %v", err)
-		}
-		if a.Size() != tt.expected {
-			t.Errorf("Array.Size() = %d, want %d", a.Size(), tt.expected)
-		}
+		assert.NoErr(t, err)
+		assert.Equal(t, a.Size(), tt.expected)
 	}
 }
+
 func TestShapeDims(t *testing.T) {
 	s := Shape{2, 3, 4}
 	expected := 3
-	if s.Dims() != expected {
-		t.Errorf("Shape.dims() = %d, want %d", s.Dims(), expected)
-	}
+	assert.Equal(t, s.Dims(), expected)
 }
 
 func TestShapeIsScalar(t *testing.T) {
 	s := Shape{}
-	if !s.IsScalar() {
-		t.Errorf("Shape.IsScalar() = false, want true")
-	}
+	assert.True(t, s.IsScalar())
+
 	s = Shape{1}
-	if s.IsScalar() {
-		t.Errorf("Shape.IsScalar() = true, want false")
-	}
+	assert.False(t, s.IsScalar())
 }
 
 func TestArrayFormat(t *testing.T) {
@@ -100,13 +88,9 @@ func TestArrayFormat(t *testing.T) {
 
 	for _, tt := range tests {
 		a, err := MakeArrayFrom(tt.shape, tt.data)
-		if err != nil {
-			t.Fatalf("MakeArrayFrom() error = %v", err)
-		}
+		assert.NoErr(t, err)
 		result := fmt.Sprintf("%v", a)
-		if result != tt.expected {
-			t.Errorf("Array.Format() = \n%v\nWant \n%v", result, tt.expected)
-		}
+		assert.Equal(t, result, tt.expected)
 	}
 	// runtime.GC() // test if finalizer is called
 }
@@ -115,9 +99,7 @@ func TestShapeFormat(t *testing.T) {
 	s := Shape{2, 3, 4}
 	expected := "(2, 3, 4)"
 	result := fmt.Sprintf("%v", s)
-	if result != expected {
-		t.Errorf("Shape.Format() = %v, want %v", result, expected)
-	}
+	assert.Equal(t, result, expected)
 }
 
 func TestMakeArray(t *testing.T) {
@@ -133,9 +115,7 @@ func TestMakeArray(t *testing.T) {
 
 	for _, tt := range tests {
 		_, err := MakeArrayFrom(tt.Shape, tt.data)
-		if (err == nil) != tt.valid {
-			t.Errorf("MakeArrayFrom(%v, %v) = %v, want valid = %v", tt.Shape, tt.data, err, tt.valid)
-		}
+		assert.Equal(t, err == nil, tt.valid)
 	}
 }
 
@@ -150,46 +130,19 @@ func TestArraySoftmax(t *testing.T) {
 	}
 
 	a, err := MakeArrayFrom(Shape{3}, []float32{1.0, 2.0, 3.0})
-	if err != nil {
-		t.Fatalf("MakeArrayFrom() error = %v", err)
-	}
+	assert.NoErr(t, err)
 	res, err := a.Softmax()
-	if err != nil {
-		t.Fatal("Array.Softmax() should have failed")
-	}
-	if !equal(res, []float32{0.0900305, 0.2447284, 0.6652409}) {
-		t.Errorf("Array.Softmax() = %v, want %v", res, []float32{0.0900305, 0.2447284, 0.6652409})
-	}
+	assert.NoErr(t, err)
+	assert.SliceNear(t, []float32{0.0900305, 0.2447284, 0.6652409}, res.ToHost().Interface().([]float32), 1e-6)
 
 	for _, tt := range tests {
 		array, err := MakeArrayFrom(tt.shape, tt.input)
-		if err != nil {
-			t.Fatalf("MakeArrayFrom(%v, %v) failed: %v", tt.shape, tt.input, err)
-		}
+		assert.NoErr(t, err)
 
 		res, err := array.Softmax()
-		if err != nil {
-			t.Fatalf("Array.Softmax() failed: %v", err)
-		}
-		if !equal(res, tt.expected) {
-			t.Errorf("Array.Softmax() =\n%v, want\n%v", res, tt.expected)
-		}
+		assert.NoErr(t, err)
+		assert.SliceNear(t, tt.expected, res.ToHost().Interface().([]float32), 1e-6)
 	}
-}
-
-func equal(a *Array, expected []float32) bool {
-	if a.Len() != len(expected) {
-		return false
-	}
-
-	d := a.ToHost()
-	for i, v := range expected {
-		if math.Abs(d.Index(i).Float()-float64(v)) > 1e-6 {
-			fmt.Printf("i: %d, v: %f, expected: %f\n", i, d.Index(i).Float(), v)
-			return false
-		}
-	}
-	return true
 }
 
 func TestArrayMatmul(t *testing.T) {
@@ -222,54 +175,33 @@ func TestArrayMatmul(t *testing.T) {
 
 	for _, tt := range tests {
 		a, err := MakeArrayFrom(tt.a, tt.aData)
-		if err != nil {
-			t.Fatalf("MakeArrayFrom(%v, %v) failed: %v", tt.a, tt.aData, err)
-		}
+		assert.NoErr(t, err)
 		b, err := MakeArrayFrom(tt.b, tt.bData)
-		if err != nil {
-			t.Fatalf("MakeArrayFrom(%v, %v) failed: %v", tt.b, tt.bData, err)
-		}
+		assert.NoErr(t, err)
 		var bias *Array
 		if tt.biasData != nil {
 			bias, err = MakeArrayFrom(tt.bias, tt.biasData)
-			if err != nil {
-				t.Fatalf("MakeArrayFrom(%v, %v) failed: %v", tt.bias, tt.biasData, err)
-			}
+			assert.NoErr(t, err)
 		}
 
 		result, err := a.Matmul(b, bias)
-		if (err != nil) != tt.expectError {
-			t.Errorf("Array.Matmul() error = %v, expectError %v", err, tt.expectError)
-			continue
-		}
+		assert.Equal(t, err != nil, tt.expectError)
 		if err != nil {
 			continue
 		}
-		if !equal(result, tt.expected) {
-			t.Errorf("Array.Matmul() = %v, want %v", result, tt.expected)
-		}
+		assert.SliceNear(t, tt.expected, result.ToHost().Interface().([]float32), 1e-6)
 	}
 }
 
 func TestArrayMatmulSoftmax(t *testing.T) {
 	a, err := MakeArrayFrom(Shape{2, 3}, []float32{1, 2, 3, 4, 5, 6})
-	if err != nil {
-		t.Fatalf("MakeArrayFrom() error = %v", err)
-	}
+	assert.NoErr(t, err)
 	b, err := MakeArrayFrom(Shape{3, 3}, []float32{1, 4, 7, 2, 5, 8, 3, 6, 9})
-	if err != nil {
-		t.Fatalf("MakeArrayFrom() error = %v", err)
-	}
+	assert.NoErr(t, err)
 	c, err := a.Matmul(b, nil)
-	if err != nil {
-		t.Fatalf("Array.Matmul() error = %v", err)
-	}
+	assert.NoErr(t, err)
 	d, err := c.Softmax()
-	if err != nil {
-		t.Fatalf("Array.Softmax() error = %v", err)
-	}
+	assert.NoErr(t, err)
 	expected := []float32{6.1289825e-06, 0.0024726081, 0.9975212, 9.3576195e-14, 3.059022e-07, 0.99999964}
-	if !equal(d, expected) {
-		t.Errorf("Array.Matmul().Softmax():\n%v, want \n%v", d, expected)
-	}
+	assert.SliceNear(t, expected, d.ToHost().Interface().([]float32), 1e-6)
 }
